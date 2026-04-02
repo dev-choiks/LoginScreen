baa5f9aae2ff2c8fcbe925b632b46f83e81a7037
Choi Kyungseo <kyungseo0612@suwon.ac.kr>
2026-04-02 12:07:28 +0900

    과제 1 코딩 미완성

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblAppName = new Label();
+            txtID = new TextBox();
+            txtPW = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("한컴 말랑말랑 Bold", 35.9999962F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblAppName.Location = new Point(141, 56);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(192, 77);
+            lblAppName.TabIndex = 0;
+            lblAppName.Text = "Login";
+            // 
+            // txtID
+            // 
+            txtID.Font = new Font("한컴 말랑말랑 Bold", 24F, FontStyle.Bold);
+            txtID.ForeColor = Color.Silver;
+            txtID.Location = new Point(68, 195);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(350, 59);
+            txtID.TabIndex = 2;
+            txtID.Text = "아이디";
+            txtID.Enter += txtID_Enter;
+            txtID.KeyDown += txtID_KeyDown;
+            txtID.Leave += txtID_Leave;
+            // 
+            // txtPW
+            // 
+            txtPW.Font = new Font("한컴 말랑말랑 Bold", 24F, FontStyle.Bold);
+            txtPW.ForeColor = Color.Silver;
+            txtPW.Location = new Point(68, 282);
+            txtPW.Name = "txtPW";
+            txtPW.Size = new Size(350, 59);
+            txtPW.TabIndex = 3;
+            txtPW.Text = "비밀번호";
+            txtPW.Enter += txtPW_Enter;
+            txtPW.KeyDown += txtPW_KeyDown;
+            txtPW.Leave += txtPW_Leave;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("한컴 말랑말랑 Bold", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnLogin.Location = new Point(68, 383);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(350, 70);
+            btnLogin.TabIndex = 1;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(486, 576);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPW);
+            Controls.Add(txtID);
+            Controls.Add(lblAppName);
+            Name = "Form1";
+            Text = "Login Screen";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblAppName;
+        private TextBox txtID;
+        private TextBox txtPW;
+        private Button btnLogin;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,79 @@ namespace LoginScreen
         {
             InitializeComponent();
         }
+
+        string myID = "admin";
+        string myPW = "superman";
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string inputID = txtID.Text;
+            string inputPW = txtPW.Text;
+            if (inputID == myID && inputPW == myPW) // 로그인 성공 조건
+            {
+                MessageBox.Show("로그인 성공!");
+            }
+            else
+            {
+                MessageBox.Show("아이디 또는 비밀번호가 잘못되었습니다.", "로그인 오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtID_Enter(object sender, EventArgs e)
+        {
+            if (txtID.Text == "아이디") // 아이디 입력란이 기본 텍스트일 때
+            {
+                txtID.Text = "";
+                txtID.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtID_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtID.Text)) // 아이디 입력란이 비어있을 때
+            {
+                txtID.Text = "아이디";
+                txtID.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtPW_Enter(object sender, EventArgs e)
+        {
+            if (txtPW.Text == "비밀번호") // 비밀번호 입력란이 기본 텍스트일 때
+            {
+                txtPW.Text = "";
+                txtPW.UseSystemPasswordChar = true;
+                txtPW.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtPW_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPW.Text)) // 비밀번호 입력란이 비어있을 때
+            {
+                btnLogin.Focus();
+                txtPW.UseSystemPasswordChar = false;
+                txtPW.Text = "비밀번호";
+                txtPW.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtID_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.Enter))
+            {
+                e.SuppressKeyPress = true;
+                txtPW.Focus();
+            }
+        }
+
+        private void txtPW_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.Enter))
+            {
+                e.SuppressKeyPress = true;
+                btnLogin.PerformClick();
+            }
+        }
     }
 }
